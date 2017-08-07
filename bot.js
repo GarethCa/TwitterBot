bfb83b96883fd7063efad4653e6f0f7a6d4f303e
GarethCa <grpc@st-andrews.ac.uk>
2017-08-07 23:07:57 +0100

    Knows When Mentioned
    Fixed the time display so that there are never any duplicate issues.
    Now can recognise being mentioned without spamming issue.

--- bot.js
+++ bot.js
@@ -4,15 +4,36 @@ var Twit = require('twit');
 var config = require('./config')
 var T = new Twit(config);
 
-//Set up a Stream
+//Set up the streams.
 var stream = T.stream('user');
 stream.on('follow',followed);
+stream.on('tweet',tweetedAt);
 
+
+//Callback function which checks each incoming tweet in feed and checks if
+//user is mentioned in it.
+function tweetedAt(tweet){
+	if(tweet){
+		var inreply = tweet['text'];
+		if(inreply!= null && inreply.includes('GarethTweetBot')){
+			console.log("You were tweeted at.")
+			console.log(tweet['text']);
+			var time = new Date(tweet.created_at);
+			var time_str = time.toLocaleString('en-GB');
+			var screen_name = tweet['user']['screen_name'];
+			sendTweet("I was mentioned by @"+screen_name +" at " +time_str);
+		}
+	}
+}
+
+//Callback function for the stream when user is followed.
 function followed(event){
 	console.log("You were followed.");
 	var name = event.source.name;
 	var screenName = event.source.screen_name;
-	sendTweet("Hey @"+screenName+" thanks for the follow, I'm a bot.");
+	var time = Date.parse(event.target['created_at']);
+	var time_str = time.toLocaleString('en-GB');
+	sendTweet("Hey @"+screenName+" thanks for the follow, I'm a bot. "+time_str);
 }
 
 
